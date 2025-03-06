f36c8eec1cd213ea659b4074ed230f28affd971f
Ahmed Alaa <ahmed.alaa4611@gmail.com>
2025-03-06 13:45:38 +0200

    Utilizing our middlewares in api.php routes

--- routes/api.php
+++ routes/api.php
@@ -9,56 +9,16 @@ use App\Http\Controllers\Api\PackageUserApiController;
 use App\Http\Controllers\Api\PeriodController;
 use App\Http\Controllers\Api\ProductApiController;
 use App\Http\Controllers\Api\ServiceApiController;
+use App\Http\Middleware\EnsureUserIsAdmin;
 use App\Http\Middleware\EnsureUserIsCustomer;
+use App\Http\Middleware\EnsureUserIsGymOwner;
+use App\Http\Middleware\EnsureUserIsStoreOwner;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-
-// Additional API routes
-Route::get('/period', [PeriodController::class, 'index']);
-Route::post('/period', [PeriodController::class, 'store']);
-Route::get('/period/{period}', [PeriodController::class, 'show']);
-Route::put('/period/{period}', [PeriodController::class, 'update'])->can('handle', 'period');
-Route::delete('/period/{period}', [PeriodController::class, 'destroy'])->can('handle', 'period');
-
-// Products
-Route::get('/products', [ProductApiController::class, 'index']);
-Route::post('/products/create', [ProductApiController::class, 'store']);
-Route::get('/products/show/{product}', [ProductApiController::class, 'show']);
-Route::put('/products/update/{product}', [ProductApiController::class, 'update']);
-Route::delete('/products/{product}', [ProductApiController::class, 'destroy']);
-
-// Package
-Route::get('/packages', [PackageApiController::class, 'index']);
-Route::post('/packages/create', [PackageApiController::class, 'store']);
-Route::get('/packages/show/{package}', [PackageApiController::class, 'show']);
-Route::put('/packages/update/{package}', [PackageApiController::class, 'update']);
-Route::delete('/packages/{package}', [PackageApiController::class, 'destroy']);
-
-// Package_User
-Route::get('/package-user', [PackageUserApiController::class, 'index']);
-Route::post('/package-user/create', [PackageUserApiController::class, 'store']);
-Route::get('/package-user/show/{id}', [PackageUserApiController::class, 'show']);
-Route::put('/package-user/update/{id}', [PackageUserApiController::class, 'update']);
-Route::delete('/package-user/{id}', [PackageUserApiController::class, 'destroy']);
-
-// Service
-Route::get('/services', [ServiceApiController::class, 'index']);
-Route::post('/services/create', [ServiceApiController::class, 'store']);
-Route::get('/services/show/{service}', [ServiceApiController::class, 'show']);
-Route::put('/services/update/{service}', [ServiceApiController::class, 'update'])->can('handle', 'service');
-Route::delete('/services/{service}', [ServiceApiController::class, 'destroy'])->can('handle', 'service');
-
-// Cart
-Route::middleware(['auth:sanctum', EnsureUserIsCustomer::class])->group(function () {
-
-    Route::get('/cart', [CartApiController::class, 'index']);
-    Route::get('/addtoCart/{id}', [CartApiController::class, 'addToCart']);
-
-});
 
 Route::middleware('guest')->group(function () {
 
@@ -78,6 +38,53 @@ Route::middleware('auth:sanctum')->group(function () {
 
     Route::controller(AuthController::class)->group(function () {
         Route::delete('/logout', 'destroy');
+    });
+
+    Route::middleware(EnsureUserIsGymOwner::class)->group(function () {
+        // Additional API routes
+        Route::get('/period', [PeriodController::class, 'index']);
+        Route::post('/period', [PeriodController::class, 'store']);
+        Route::get('/period/{period}', [PeriodController::class, 'show']);
+        Route::put('/period/{period}', [PeriodController::class, 'update'])->can('handle', 'period');
+        Route::delete('/period/{period}', [PeriodController::class, 'destroy'])->can('handle', 'period');
+
+        // Service
+        Route::get('/services', [ServiceApiController::class, 'index']);
+        Route::post('/services/create', [ServiceApiController::class, 'store']);
+        Route::get('/services/show/{service}', [ServiceApiController::class, 'show']);
+        Route::put('/services/update/{service}', [ServiceApiController::class, 'update'])->can('handle', 'service');
+        Route::delete('/services/{service}', [ServiceApiController::class, 'destroy'])->can('handle', 'service');
+    });
+
+    Route::middleware(EnsureUserIsStoreOwner::class)->group(function () {
+        // Products
+        Route::get('/products', [ProductApiController::class, 'index']);
+        Route::post('/products/create', [ProductApiController::class, 'store']);
+        Route::get('/products/show/{product}', [ProductApiController::class, 'show']);
+        Route::put('/products/update/{product}', [ProductApiController::class, 'update']);
+        Route::delete('/products/{product}', [ProductApiController::class, 'destroy']);
+    });
+
+    Route::middleware(EnsureUserIsAdmin::class)->group(function () {
+        // Package
+        Route::get('/packages', [PackageApiController::class, 'index']);
+        Route::post('/packages/create', [PackageApiController::class, 'store']);
+        Route::get('/packages/show/{package}', [PackageApiController::class, 'show']);
+        Route::put('/packages/update/{package}', [PackageApiController::class, 'update']);
+        Route::delete('/packages/{package}', [PackageApiController::class, 'destroy']);
+
+        // Package_User
+        Route::get('/package-user', [PackageUserApiController::class, 'index']);
+        Route::post('/package-user/create', [PackageUserApiController::class, 'store']);
+        Route::get('/package-user/show/{id}', [PackageUserApiController::class, 'show']);
+        Route::put('/package-user/update/{id}', [PackageUserApiController::class, 'update']);
+        Route::delete('/package-user/{id}', [PackageUserApiController::class, 'destroy']);
+    });
+
+    Route::middleware(EnsureUserIsCustomer::class)->group(function () {
+        // Cart
+        Route::get('/cart', [CartApiController::class, 'index']);
+        Route::get('/addtoCart/{id}', [CartApiController::class, 'addToCart']);
     });
 
 });
